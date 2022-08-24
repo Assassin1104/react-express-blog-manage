840b79452fcac6056ce528c1124d3b4acccbe420
King <kingdev10260522@gmail.com>
2022-08-24 16:54:47 -0700

    commit

--- front-end/src/screens/authentication/Header.jsx
+++ front-end/src/screens/authentication/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Navbar, Nav } from "react-bootstrap";
-import {BasketFill} from 'react-bootstrap-icons';
 import logo from "../../assets/images/logo.svg";
 import "../../assets/css/main.css";
 import { Button } from "react-bootstrap";
@@ -11,15 +10,18 @@ import cart from '../../assets/images/cart.svg';
 export default function Header() {
     return(
         <div className="header">
-            <Navbar bg="white" variant="light">
+            <Navbar bg="white" variant="light" expand="lg">
                 <Container>
                     <Navbar.Brand href="#home"><img className="logo" src={logo}/></Navbar.Brand>
-                    <Nav className="me-auto">
-                        <img className="cart me-2" src={cart} />
-                        <Nav.Link href="#home" className="me-2"><Button variant="danger" className="primary-background navbar-font">Become a Local Expert</Button></Nav.Link>
-                        <Nav.Link href="#features" className="me-2 primary-color">Login</Nav.Link>
-                        <Nav.Link href="#pricing" className="me-2"><Button variant="danger" className="primary-background navbar-font">Sign Up</Button></Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <img className="cart me-2" src={cart} />
+                            <Nav.Link href="#home" className="me-2"><Button variant="danger" className="primary-background navbar-font">Become a Local Expert</Button></Nav.Link>
+                            <Nav.Link href="#features" className="me-2 primary-color">Login</Nav.Link>
+                            <Nav.Link href="#pricing" className="me-2"><Button variant="danger" className="primary-background navbar-font">Sign Up</Button></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
--- front-end/src/screens/authentication/Register.jsx
+++ front-end/src/screens/authentication/Register.jsx
@@ -1,14 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import { Row, Col, Form ,Button } from "react-bootstrap";
+import { BiEnvelope } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.svg";
+import google from "../../assets/images/google.png";
+import facebook from "../../assets/images/facebook.png";
+
 
 export default function Register() {
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleFirstNameChange = (e) => {
+        setFirstname(e.target.value);
+    }
+    const handleLastNameChange = (e) => {
+        setLastname(e.target.value);
+    }
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+    const handleRegisterFormSubmit = (e) => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {  
+            return;
+        }
+        console.log("Asdfasdfasdf");
+    }
     return (
-        <div className="register row">
-            <div className="col-sm-6 register-form">
-                <div className="form-title d-flex justify-content-center">Create an account</div>
-            </div>
-            <div className="col-sm-6 register-background">
-                
-            </div>
-        </div>
+        <Row className="register">
+            <Col sm={12} md={6} className="register-form d-flex align-items-center">
+                <Row>
+                    <Col sm={12}>
+                        <Form onSubmit={handleRegisterFormSubmit}>
+                            <Row>
+                                <Form.Label className="login-title">Create an account</Form.Label>
+                                <Form.Label>Already an account? Login</Form.Label>
+                                <Col md={6} sm={12} className="pe-1 mt-3">
+                                    <Form.Control placeholder="First name" className="py-3" onChange={handleFirstNameChange} required/>
+                                </Col>
+                                <Col md={6} sm={12} className="ps-1 mt-3">
+                                    <Form.Control placeholder="Last name" className="py-3" onChange={handleLastNameChange} required/>
+                                </Col>
+                                <Col sm={12} className="mt-3">
+                                    <Form.Control placeholder="email" className="py-3" onChange={handleEmailChange} required/>
+                                </Col>
+                                <Col sm={12} className="mt-3">
+                                    <Form.Control placeholder="password" type="password" className="py-3" onChange={handlePasswordChange} required/>
+                                </Col>
+                                <Col sm={12} className="mt-3">
+                                    <Button variant="light" className="primary-color py-3" type="submit"> Create Your Account&nbsp;&nbsp;&nbsp;<BiEnvelope size={19}/> </Button>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Col>
+                    <Col sm={12}>
+                        <div className="external-title mt-3">or sign up with</div>
+                        <div className="button-group d-flex justify-content-center">
+                            <Button variant="light" className="me-3"><img src={google}/></Button>
+                            <Button variant="light" className="ms-3"><img src={facebook}/></Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Col>
+            <Col sm={12} md={6} className="register-background">
+                <div className="register-opicity">
+                    <img className="logo-register" src={logo}/>
+                </div>
+            </Col>
+        </Row>
     );
 } 
